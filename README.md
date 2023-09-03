2b6d5d4e6f0d9a78ce4a812af4b2bdd947019ae5
JF <80600490+Iam0ti@users.noreply.github.com>
2023-09-03 17:12:32 +0100

    feat: added the optparse for options

---
+++ README.md
@@ -0,0 +1,24 @@
+# Morse Decoder  & Encoder 
+
+A Python script to decode plain english to Morse code and vice verse .
+
+## Requirements
+
+* Python 3.x
+* `optparse` library
+
+## Usage
+
+The script takes three command-line arguments: `-d` (or `--encode`) to encode Plain english to Morse code, and `-e` (or `--encode`) to decode  Morse code to Plain english.
+
+Example usage:
+
+```
+python3 mmorse_decoder_encoder.py -d
+```
+
+## Future Improvements
+
+The following improvements could be made to the script in the future:
+
+* ADD THE SUPPORT OF PDF AND TXT.
--- script.py
+++ script.py
@@ -1,14 +1,4 @@
 """AUTHOR : OTI """
-import subprocess
-
-
-
-to_encode = input('ENTER THE WORD TO ENCODE : ').upper()
-
-input_to_decode = input('ENTER THE WORD TO DECODE :')
-to_decode = input_to_decode.split()
-  
-#print(f'{to_encode} first')
 # Define a dictionary for Morse code mapping
 morse_code_dict = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
@@ -24,24 +14,23 @@ morse_code_dict = {
 # Create a reverse dictionary to map Morse code back to characters
 reverse_morse_code_dict = {v: k for k, v in morse_code_dict.items()}
 
-def morse_encoder(char_list):
+def morse_encoder():
+    to_encode = input('ENTER THE WORD TO ENCODE : ').upper()
     encoded_list = []
-    for char in char_list:
+    for char in to_encode:
          for chrr in char:
             if chrr in morse_code_dict.keys():
                 encoded_list.append(morse_code_dict.get(chrr))
                 print(morse_code_dict.get(chrr))
     encoded_str = ' '.join(encoded_list)
-    print(f'THE word {char_list} is decoded as {encoded_str}')
+    print(f'THE word {to_encode} is decoded as {encoded_str}')
 
-def morse_decoder(char_list):
+def morse_decoder():
+    input_to_decode = input('ENTER THE WORD TO DECODE :')
+    to_decode = input_to_decode.split()
     decoded_list = []
-    for char in char_list:
+    for char in to_decode:
          if char in reverse_morse_code_dict.keys():
             decoded_list.append(reverse_morse_code_dict.get(char))
     decoded_str = ' '.join(decoded_list)
-    print(f'THE MORSE code  {char_list} is decoded as {decoded_str}')    
-
-
-morse_encoder(to_encode)
-morse_decoder(to_decode)
+    print(f'THE MORSE code  {to_decode} is decoded as {decoded_str}')    
