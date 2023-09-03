7c8cd7594ee3b398c6152a599f3d439aeea7dded
JF <80600490+Iam0ti@users.noreply.github.com>
2023-09-03 15:45:39 +0100

    feat: added the main functions

--- script.py
+++ script.py
@@ -1,11 +1,14 @@
 """AUTHOR : OTI """
 import subprocess
-import sys 
 
 
 
+to_encode = input('ENTER THE WORD TO ENCODE : ').upper()
 
-
+input_to_decode = input('ENTER THE WORD TO DECODE :')
+to_decode = input_to_decode.split()
+  
+#print(f'{to_encode} first')
 # Define a dictionary for Morse code mapping
 morse_code_dict = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
@@ -21,10 +24,24 @@ morse_code_dict = {
 # Create a reverse dictionary to map Morse code back to characters
 reverse_morse_code_dict = {v: k for k, v in morse_code_dict.items()}
 
-### push to github nhnnhnhn
-char = 'A'
-morse_code = morse_code_dict[char]
-decoded_char = reverse_morse_code_dict[morse_code]
+def morse_encoder(char_list):
+    encoded_list = []
+    for char in char_list:
+         for chrr in char:
+            if chrr in morse_code_dict.keys():
+                encoded_list.append(morse_code_dict.get(chrr))
+                print(morse_code_dict.get(chrr))
+    encoded_str = ' '.join(encoded_list)
+    print(f'THE word {char_list} is decoded as {encoded_str}')
 
-print(f"{char} is encoded as {morse_code}")
-print(f"{morse_code} is decoded as {decoded_char}")
+def morse_decoder(char_list):
+    decoded_list = []
+    for char in char_list:
+         if char in reverse_morse_code_dict.keys():
+            decoded_list.append(reverse_morse_code_dict.get(char))
+    decoded_str = ' '.join(decoded_list)
+    print(f'THE MORSE code  {char_list} is decoded as {decoded_str}')    
+
+
+morse_encoder(to_encode)
+morse_decoder(to_decode)
